b1afe6d12cffaa32674145e9333b0a944870f43a
Nathan Shin <nathanshin@utexas.edu>
2016-04-25 02:49:39 -0500

    Add nav animations

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var github_nav = document.getElementById('github_nav');
 var dribbble_nav = document.getElementById('dribbble_nav');
 var twitter_nav = document.getElementById('twitter_nav');
 var email_nav = document.getElementById('email_nav');
+var about_nav = document.getElementById('about_nav')
+var blog_nav = document.getElementById('blog_nav')
+var resume_nav = document.getElementById('resume_nav')
 
 
 
@@ -36,4 +39,22 @@ twitter_nav.addEventListener("mouseover", function() {
 email_nav.addEventListener("mouseover", function() {
 	background.style.backgroundImage = "url(img/email_nav.png)";
 	background.style.backgroundPosition = "532px -4px"
+});
+
+// About navigation
+about_nav.addEventListener("mouseover", function() {
+	background.style.backgroundImage = "url(img/nav_nav.png)";
+	background.style.backgroundPosition = "699px 36px"
+});
+
+// Blog navigation
+blog_nav.addEventListener("mouseover", function() {
+	background.style.backgroundImage = "url(img/nav_nav.png)";
+	background.style.backgroundPosition = "699px 36px"
+});
+
+// Resume navigation
+resume_nav.addEventListener("mouseover", function() {
+	background.style.backgroundImage = "url(img/nav_nav.png)";
+	background.style.backgroundPosition = "699px 36px"
 });
